b6934c107aa8165db287a576818a06e46142ac9b
Royce Duong <royceduong@Royces-MBP.attlocal.net>
2018-07-14 13:28:40 -0700

    render

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'first_app/home.html')
     
 def test(request):
     return HttpResponse("Test Page")
