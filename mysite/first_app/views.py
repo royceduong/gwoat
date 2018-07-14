7c6a4150afe657b6dfb9fb4a784614a18509766c
Royce Duong <royceduong@Royces-MBP.attlocal.net>
2018-07-14 14:15:10 -0700

    basic form

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .forms import UploadFileForm
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'first_app/home.html')
     
+def process(request):
+    if request.method == 'POST':
+        # form = DocumentForm(request.POST, request.FILES)
+        print(request.FILES)
+    return redirect('/')
 def test(request):
     return HttpResponse("Test Page")
+
