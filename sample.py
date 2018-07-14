78aa085820ac74323480465b544c87ba6471dd1c
Jonathan Hightower <jon.hightower.310@gmail.com>
2018-07-14 13:31:15 -0700

    Changed backslash to forward slash

--- sample.py
+++ sample.py
@@ -4,13 +4,13 @@ def ConvertToRGB(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
-img = cv2.imread("data\peeps.jpg")
+img = cv2.imread("data/signu.jpg")
 resized_img = cv2.resize(img, (920, 480))
 gray_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
 
 img_copy = gray_img.copy()
 
-classifier = cv2.CascadeClassifier('data\haarcascade_frontalface_alt.xml')
+classifier = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
 
 faces = classifier.detectMultiScale(img_copy, scaleFactor=1.1, minNeighbors=5)
 
@@ -20,9 +20,5 @@ for (x, y, w, h) in faces:
 cv2.imshow('image', resized_img)
 
 print(faces)
-
-#cv2.imshow('image', resized_img)
-
-#cv2.
 
 cv2.waitKey(0)
