5fbbcc32850613388f842e58fdd59bd8f69050cc
Jonathan Hightower <jon.hightower.310@gmail.com>
2018-07-14 20:45:39 -0700

    Goats

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -3,16 +3,20 @@ from django.http import HttpResponse
 import cv2
 import os
 from django.core.files.storage import FileSystemStorage
+from django.contrib.staticfiles.templatetags.staticfiles import static
+
 
 def draw_on_faces(filepath): #path to image
 
     file = os.path.basename(filepath)
     file = os.path.splitext(file)[0]
-    print (file)
-    print (filepath)
     img = cv2.imread(filepath)
-    #cv2.imshow('image', img)
-    #cv2.waitKey(0)
+
+    goat_img_path = "media/goats/1.jpg"
+    goat_img = cv2.imread(goat_img_path)
+
+
+
     resized_img = cv2.resize(img, (920, 480))
     gray_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
 
@@ -23,14 +27,18 @@ def draw_on_faces(filepath): #path to image
     faces = classifier.detectMultiScale(img_copy, scaleFactor=1.1, minNeighbors=5)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(resized_img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+        #cv2.rectangle(resized_img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+        resized_goat = cv2.resize(goat_img, (w,h))
+        for a in range(0, 920): #row
+            for b in range(0, 480): #column
+                if (a > x and a < x+w and (b > y and b < y + h)):
+                    goat_a = x - a
+                    goat_b = y - b
+                    resized_img[b,a] = resized_goat[goat_b, goat_a]
+
 
     new_file =  "new_" + file + '.jpg'
     cv2.imwrite("media/" + new_file, resized_img)
-
-    #cv2.imwrite('blabla.jpeg', resized_img)
-
-    #cv2.waitKey(0)
 
     return new_file
 
@@ -45,9 +53,6 @@ def index(request):
         file_url = fs.path(filename)
 
         new_file = draw_on_faces(file_url)
-
-        print("File Received")
-        print (new_file)
         return render(request, 'first_app/result.html', {'drawn_on_file': new_file})
 
     return render(request, 'first_app/home.html')
