c20286ddc4f66f33d852a94b77356447616e487c
Jonathan Hightower <jon.hightower.310@gmail.com>
2018-07-14 21:26:31 -0700

    Putting it together

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -4,6 +4,7 @@ import cv2
 import os
 from django.core.files.storage import FileSystemStorage
 from django.contrib.staticfiles.templatetags.staticfiles import static
+import random
 
 
 def draw_on_faces(filepath): #path to image
@@ -11,11 +12,6 @@ def draw_on_faces(filepath): #path to image
     file = os.path.basename(filepath)
     file = os.path.splitext(file)[0]
     img = cv2.imread(filepath)
-
-    goat_img_path = "media/goats/1.jpg"
-    goat_img = cv2.imread(goat_img_path)
-
-
 
     resized_img = cv2.resize(img, (920, 480))
     gray_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
@@ -27,14 +23,19 @@ def draw_on_faces(filepath): #path to image
     faces = classifier.detectMultiScale(img_copy, scaleFactor=1.1, minNeighbors=5)
 
     for (x, y, w, h) in faces:
+        w_scaled = int(w * 1.5)
+        h_scaled = int(h * 1.5)
+        num =  random.randint(1, 7)
+        goat_img = cv2.imread("media/goats/" + str(num) + '.jpg')
         #cv2.rectangle(resized_img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
-        resized_goat = cv2.resize(goat_img, (w,h))
+        resized_goat = cv2.resize(goat_img, (w_scaled,h_scaled))
         for a in range(0, 920): #row
             for b in range(0, 480): #column
-                if (a > x and a < x+w and (b > y and b < y + h)):
+                if (a > x and a < x+w_scaled and (b > y and b < y + h_scaled)):
                     goat_a = x - a
                     goat_b = y - b
-                    resized_img[b,a] = resized_goat[goat_b, goat_a]
+                    if (resized_goat[goat_b, goat_a][0] != 0 and resized_goat[goat_b, goat_a][1] != 0 and resized_goat[goat_b, goat_a][2] != 0):
+                        resized_img[b,a] = resized_goat[goat_b, goat_a]
 
 
     new_file =  "new_" + file + '.jpg'
