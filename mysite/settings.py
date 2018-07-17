4ef6739f2314983342f2bb274678b9f741f66a20
Jonathan Hightower <jon.hightower.310@gmail.com>
2018-07-16 22:25:51 -0700

    Print is awesome

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 print("OS CWD: " + os.getcwd())
 if os.getcwd() == '/app':
