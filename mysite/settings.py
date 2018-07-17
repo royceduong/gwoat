64e3e5bb39fd33274fa53316b41c4134cde7196c
Jonathan Hightower <jon.hightower.310@gmail.com>
2018-07-16 22:33:22 -0700

    added more debug settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,13 +124,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+rint("BASE_DIR Before: " + BASE_DIR)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+print("BASE_DIR After: " + BASE_DIR)
+print ("ROOT: " + STATIC_ROOT)
+print("DIRS: " + str(STATICFILES_DIRS))
+print ("URL: " + STATIC_URL)
 print("OS CWD: " + os.getcwd())
+
 if os.getcwd() == '/app':
     import dj_database_url
     DATABASES = {
