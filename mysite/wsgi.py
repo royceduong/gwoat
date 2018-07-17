2523825a67786026713e9d9f4b891cb9e668fa1d
Jonathan Hightower <jon.hightower.310@gmail.com>
2018-07-16 23:22:08 -0700

    random debug message

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+from dj_static import Cling, MediaCling #really important library
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
